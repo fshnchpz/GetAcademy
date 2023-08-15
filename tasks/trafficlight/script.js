9ac0a47e8f57242be0d8fc3d0e902f1878855cd2
Johnny D Truong <little_joe123@hotmail.com>
2023-08-15 11:01:26 +0200

    Added traffic light task

--- tasks/trafficlight/script.js
+++ tasks/trafficlight/script.js
@@ -1,6 +1,50 @@
-function turnLight(off1, off2, on1, on2){
-    document.getElementById(off1).classList.remove("active");
-    document.getElementById(off2).classList.remove("active");
-    document.getElementById(on1).classList.add("active");
-    document.getElementById(on2).classList.add("active");
+var turnReady = true;
+var currentLight = 'green';
+
+function turnAllOff() {
+    document.getElementById('green').innerHTML = "";
+    document.getElementById('red').innerHTML = "";
+    document.getElementById('yellow').innerHTML = "";
+}
+
+function turnLight(color){
+    turnAllOff();
+    
+    document.getElementById(color).innerHTML = `<div id="${color}" class="light_active ${color}_active"></div>`;
+}
+
+function turnRed() {
+    if (turnReady) {
+        turnReady = false;
+        setTimeout(goRed, 2000);
+        turnReady = true;
+        currentLight = 'red';
+    }
+}
+function turnYellowRed() {
+    if (turnReady) {
+        turnReady = false;
+        setTimeout(goYellowRed, 1000);
+        turnReady = true;
+        currentLight = 'red';
+    }
+}
+function turnGreen() {
+    if (turnReady) {
+        turnReady = false;
+        setTimeout(goGreen, 2000);
+        turnReady = true;
+        currentLight = 'green';
+    }
+}
+
+function goRed() {
+    turnLight('red');
+}
+function goYellowRed() {
+    turnLight('yellow');
+    setTimeout(goRed, 1000);
+}
+function goGreen() {
+    turnLight('green');
 }
