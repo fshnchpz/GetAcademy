62399c5df260312d5ba0fe2daf675af4fad46216
Johnny D Truong <little_joe123@hotmail.com>
2023-08-28 19:31:42 +0200

    Update script.js

--- tasks/pokemon_game/script.js
+++ tasks/pokemon_game/script.js
@@ -22,7 +22,8 @@ let pkmn_party = [
     {"id":"blastoise","name":"Blastoise","type":["Water"],"curHP":268, "maxHP":268,"attack":153,"defense":184,"sp_atk":157,"sp_def":193,"speed":144,"moves":["Hydro Pump"]},
     {"id":"charizard","name":"Charizard","type":["Fire"],"curHP":266, "maxHP":266,"attack":155,"defense":144,"sp_atk":200,"sp_def":157,"speed":184,"moves":["Fire Blast"]},
     {"id":"Flygon","name":"Flygon","type":["Ground","Dragon"],"curHP":270, "maxHP":270,"attack":184,"defense":148,"sp_atk":148,"sp_def":148,"speed":184,"moves":["Dragon Rush"]},
-    {"id":"Rotom-frost","name":"Rotom","type":["Electric","Ice"],"curHP":210, "maxHP":210,"attack":121,"defense":197,"sp_atk":193,"sp_def":197,"speed":159,"moves":["Thunder","Blizzard"]}
+    {"id":"Rotom-frost","name":"Rotom","type":["Electric","Ice"],"curHP":210, "maxHP":210,"attack":121,"defense":197,"sp_atk":193,"sp_def":197,"speed":159,"moves":["Thunder","Blizzard"]},
+    {"id":"Lucario","name":"Lucario","type":["Fighting","Steel"],"curHP":250, "maxHP":250,"attack":202,"defense":130,"sp_atk":211,"sp_def":130,"speed":166,"moves":["Close Combat","Meteor Mash","Aura Sphere","Flash Cannon"]}
 ];
 const data_moves = [
     { "id":"Solar Beam", "Power": 120, "mType":"Grass", "Special":true,"Accuracy":100},
@@ -45,7 +46,12 @@ const data_moves = [
     { "id":"Dragon Rush", "Power": 120, "mType":"Dragon", "Special":false,"Accuracy":75},
     
     { "id":"Psychic", "Power": 90, "mType":"Psychic", "Special":true,"Accuracy":100},
-    { "id":"Psychic Fangs", "Power": 85, "mType":"Psychic", "Special":false,"Accuracy":100}
+    { "id":"Psychic Fangs", "Power": 85, "mType":"Psychic", "Special":false,"Accuracy":100},
+
+    { "id":"Meteor Mash", "Power": 90, "mType":"Steel", "Special":false,"Accuracy":90},
+    { "id":"Flash Cannon", "Power": 80, "mType":"Steel", "Special":true,"Accuracy":100},
+    { "id":"Close Combat", "Power": 120, "mType":"Fighting", "Special":false,"Accuracy":100},
+    { "id":"Aura Sphere", "Power": 80, "mType":"Fighting", "Special":true,"Accuracy":100}
 ];
 
 let enemy_party = [];
