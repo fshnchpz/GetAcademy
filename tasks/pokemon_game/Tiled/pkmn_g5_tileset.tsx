4bd5826bbe0e029d9848f52ea222e9787cc378b5
Johnny D Truong <little_joe123@hotmail.com>
2023-09-08 00:27:49 +0200

    .

---
+++ tasks/pokemon_game/Tiled/pkmn_g5_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="pkmn_g5_tileset" tilewidth="32" tileheight="32" tilecount="7000" columns="8">
+ <image source="../Images/pkmn_g5_tileset.png" width="256" height="28000"/>
+</tileset>
